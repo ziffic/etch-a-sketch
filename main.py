1fd0cf358b8644849513cec104a1e21bf0fec472
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-13 10:44:55 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def move_left():
+    tim.left(10)
+    tim.forward(10)
+
+
+def move_right():
+    tim.right(10)
+    tim.forward(10)
+
+
+def clear():
+    tim.clear()
+    tim.reset()
+
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="d", fun=move_right)
+screen.onkey(key="c", fun=clear)
+screen.exitonclick()
